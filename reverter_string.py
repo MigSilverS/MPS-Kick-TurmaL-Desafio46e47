08a8010ca863879b67022e4193585cc990bef027
MigSilverS <migupoke@gmail.com>
2024-09-03 20:59:59 -0300

    inserção no github

---
+++ reverter_string.py
@@ -0,0 +1,5 @@
+def reverter_string(texto):
+    return texto[::-1]
+
+texto = input("Digite o textinho: ")
+print(reverter_string(texto))
---
+++ soma_numeros.py
@@ -0,0 +1,6 @@
+def soma_numeros(num1, num2):
+    return num1 + num2
+
+numero1 = input("Digite o primeiro número:")
+numero2 = input("Digite o segundo número:")
+print(soma_numeros(int(numero1), int(numero2)))
